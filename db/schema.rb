0d833f12b844f0299ca4127d9f92e917bdbc50e4
Michael Li <limenyon@hotmail.com>
2017-08-18 14:28:11 +0100

    starting pulse landing

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817121222) do
+ActiveRecord::Schema.define(version: 20170818102630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
+    t.string "photo"
   end
 
   create_table "doses", force: :cascade do |t|
